d0f677ec9d4e5bd715532ca52b80aeed40bbb54e
dayanandrekha <rekhadayanand2019@gmail.com>
2026-01-17 10:04:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "jenkinsvm"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1ms"
+  size                = "Standard_D2s_v3"
   admin_username      = "azureuser"
 
   network_interface_ids = [
