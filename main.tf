bf8993dca7d04dd715b19754ef4b8e0b5d53de48
dayanandrekha <rekhadayanand2019@gmail.com>
2026-01-17 09:50:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,12 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "default"
+  name                 = "jenkins-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
 
 resource "azurerm_network_interface" "nic" {
   name                = "jenkins-nic"
