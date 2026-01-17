1758b924cdb2bae16af67a0dc03195e84fb5a47a
dayanandrekha <rekhadayanand2019@gmail.com>
2026-01-17 10:01:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_subnet" "subnet" {
 
 
 resource "azurerm_network_interface" "nic" {
-  name                = "jenkins-nic"
+  name                = "jenkins-nic-2"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
